a9df837b8f6a3f6724154fb5ad9faf1871a379b8
Tokari <42688334+tkrii@users.noreply.github.com>
2024-05-21 14:38:08 -0600

    add createArgo ... Theme

--- lib/argo.dart
+++ lib/argo.dart
@@ -5,4 +5,6 @@ export 'src/colors/utilities/brightness.dart';
 export 'src/colors/utilities/color.dart';
 export 'src/colors/utilities/color_scheme.dart';
 export 'src/inherited_theme.dart';
+export 'src/themes/dark_theme.dart';
+export 'src/themes/light_theme.dart';
 export 'src/themes/variant.dart';
